3facef42a866b6589f4257162a06e2fa181605b1
JU4Nkpl <130647266+JU4Nkpl@users.noreply.github.com>
2023-06-01 10:45:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(input.temperature())
+})
+let humedad = 0
+radio.setGroup(115)
+basic.forever(function () {
+    humedad = 0
+})
